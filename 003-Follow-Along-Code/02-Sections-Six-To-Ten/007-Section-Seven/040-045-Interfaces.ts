be89db9b27b52b92b4ce78e34611158b0f28cc2b
Ryan Smith <125925904+ryansmith122000@users.noreply.github.com>
2023-07-12 16:31:18 -0700

    added new notes/typescript files

---
+++ 003-Follow-Along-Code/02-Sections-Six-To-Ten/007-Section-Seven/040-045-Interfaces.ts
@@ -0,0 +1,54 @@
+interface Vehicle {
+    name: string;
+    year: Date;
+    runs: boolean;
+    summary(): string;
+};
+
+const oldCivic = {
+    name: 'civic',
+    year: new Date(),
+    runs: true,
+    summary(): string {
+        return `Name is: ${this.name} Year is: ${this.year} Does it run?: ${this.runs}`;
+    },
+};
+
+const printVehicle = ( vehicle: Vehicle ): void => {
+    console.log(vehicle.summary);
+};
+
+printVehicle(oldCivic);
+
+interface Reportable {
+    summary(): string;
+};
+
+const drink = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40,
+    summary(): string {
+        return `My drink has ${this.sugar}g of sugar.`;
+    },
+};
+    // both drink and newVehicle are Reportable type
+const newVehicle = {
+    name: 'tesla',
+    year: new Date(),
+    runs: true,
+    summary(): string {
+        return `Name is: ${this.name} Year is: ${this.year} Does it run?: ${this.runs}`;
+    },
+};
+
+const printSummary = (item: Reportable): void => {
+    console.log(newVehicle.summary);
+};
+
+printSummary(newVehicle);
+printSummary(drink);
+
+// if an interface is generic, it can be reused for different objects. newVehicle and drink are not closely related except for a summary, which allows them to have the reportable type.
+
+
